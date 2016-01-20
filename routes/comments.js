4d47b40060635615bdeff38f83f32413e002a510
Teisia Park <teisia@gmail.com>
2016-01-20 11:13:36 -0700

    showing comments page

--- routes/comments.js
+++ routes/comments.js
@@ -2,7 +2,18 @@ var express = require('express');
 var router = express.Router();
 var knex = require('../db/knex');
 
+function Posts() {
+  return knex('posts');
+}
 
+function Comments() {
+  return knex('comments');
+}
 
+router.get('/:post_id/comments', function(req, res, next) {
+  Comments().where('id', req.params.id).then(function (comments) {
+    res.json({'SUCCESS': comments});
+  })
+});
 
 module.exports = router;
