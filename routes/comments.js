27cfd480bb8e09a93f6defda62f890b4e72cca10
Teisia Park <teisia@gmail.com>
2016-01-20 11:31:00 -0700

    showing individual comment page

--- routes/comments.js
+++ routes/comments.js
@@ -11,8 +11,8 @@ function Comments() {
 }
 
 router.get('/:post_id/comments', function(req, res, next) {
-  Comments().where('id', req.params.id).then(function (comments) {
-    res.json({'SUCCESS': comments});
+  Comments().where('post_id', req.params.post_id).then(function (comments) {
+    res.json({'SUCCESS': 'This is the comments page'});
   })
 });
 
@@ -24,6 +24,14 @@ var newComment = {
 }
   Comments().insert(newComment).then(function (comment) {
     res.redirect('/'+req.params.post_id+'/comments');
+  })
+});
+
+router.get('/:post_id/comments/:comment_id', function(req, res, next) {
+  Comments().where('post_id', req.params.post_id).first().then(function (post) {
+    Comments().where('id', req.params.comment_id).then(function (comment) {
+    res.json({'SUCCESS': 'This is the individual comment page'});
+    })
   })
 });
 
